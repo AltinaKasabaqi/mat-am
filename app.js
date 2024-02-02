650b1b4a544fc0a5d35c2203d86942623fd45b9b
AltinaKasabaqi <altinakasabaqi@gmail.com>
2024-02-02 15:24:43 +0100

    metoda post per rezervimet(shtimi i rezervimeve ne db)

--- app.js
+++ app.js
@@ -1,20 +1,54 @@
 const express = require('express');
 const path = require('path');
-
-
+const dbconn = require('./dbconn');
 const app = express();
 const port = 3000;
 
-// Vendos rrugën për të shërbyer skedarët statikë
+// Configure the PostgreSQL connection pool
+
+
+// Middleware to parse form data
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files
 app.use(express.static(path.join(__dirname, 'assets')));
 
-
-// Rruga për të shfaqur faqen HTML në rrugën kryesore
+// Route to serve the HTML page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'assets', 'views', 'index.html'));
 });
 
-// Dëgjo për kërkesa në portin e caktuar
+// Route to handle form submission
+// Route to handle form submission
+app.post('/', async (req, res) => {
+    try {
+      const { name, email, phone, numpeople, date, time, message } = req.body;
+  
+      // SQL query to insert data into the database
+      const query = `
+        INSERT INTO rezervimi (klienti, email, nr_tel, nr_personave, data, ora, mesazhi)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+      `;
+  
+      // Execute the query with user input values
+      dbconn.query(query, [name, email, phone, numpeople, date, time, message], (error, result) => {
+        if (error) {
+          console.error('Error creating reservation:', error);
+          res.status(500).json({ error: 'An error occurred while processing your request' });
+        } else {
+            console.log("Success");
+            res.redirect('/');
+        }
+      });
+    } catch (error) {
+      console.error('Error creating reservation:', error);
+      res.status(500).json({ error: 'An error occurred while processing your request' });
+    }
+});
+
+
+// Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
+
